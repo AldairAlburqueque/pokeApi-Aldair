862481a462b510e1fd8d5e469385203066a43b10
AldairAlburqueque <aldairalburqueque8@gmail.com>
2023-06-19 21:33:54 -0500

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,39 @@ import Home  from './pages/Home'
 import ProtectedRoutes from './pages/ProtectedRoutes'
 import Pokedex from './pages/Pokedex'
 import PokeInfo from './pages/PokeInfo'
+import { useEffect, useState } from 'react'
 
 function App() {
-  
-  return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route element={<ProtectedRoutes />}>
-          <Route path='/pokedex' element={<Pokedex />}/>
-          <Route path='/pokedex/:id' element={<PokeInfo />}/>
-        </Route>
-      </Routes>
-    </div>
-  )
-}
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      setLoading(false);
+    },2000);
+  }, []);
+
+
+    return (
+        <div className="App">
+          { loading ? (
+            <div className='loading'>
+              <img src={'./images/pikachu.png'} alt="" />
+            </div>
+            
+          ) : (
+            <Routes>
+              <Route path='/' element={<Home/>}/>
+              <Route element={<ProtectedRoutes />}>
+              <Route path='/pokedex' element={<Pokedex />}/>
+              <Route path='/pokedex/:id' element={<PokeInfo />}/>
+            </Route>
+          </Routes>
+          )
+            
+          }
+      </div>
+    )
+  }
+
 
 export default App
